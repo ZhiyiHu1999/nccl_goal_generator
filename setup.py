2004167c5c00501a90af1ab33e6f61ae3bb4a84d
ZhiyiHu1999 <huzhiyi1037@gmail.com>
2025-02-10 02:33:01 +0100

    include npkit benchmark data

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     # author_email="your_email@example.com",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={
+        "nccl_goal_generator": ["npkit_data_summary_Simple.json", "npkit_data_summary_LL.json"],
+    },
+    include_package_data=True,  # include npkit_data_summary_Simple.json and npkit_data_summary_LL.json
     entry_points={
         "console_scripts": [
             "nccl_goal_generator = nccl_goal_generator.run_generator:main",
